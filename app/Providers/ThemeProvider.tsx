04cb25706be650408fe94a9b247a3b0758304ac5
Mollalign <molledan69@gmail.com>
2025-07-17 17:09:01 +0300

    forecast data from server

--- app/Providers/ThemeProvider.tsx
+++ app/Providers/ThemeProvider.tsx
@@ -2,10 +2,17 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { GlobalContextProvider } from "../context/globalContext"
 
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider {...props}>
+      <GlobalContextProvider>
+        {children}
+      </GlobalContextProvider>
+    </NextThemesProvider>
+  )
 }
